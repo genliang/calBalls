c4880bd25760a6033cbc294ec3447ddbe704ad69
genliang <lim_genliang@hotmail.com>
2016-06-21 23:11:42 +0800

    Added combinations view

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('starter', ['ionic', 'ui.router']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,6 +21,21 @@ app.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
+});
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/main");
+  $stateProvider
+    .state('main',{
+      url: '/main',
+      templateUrl: 'views/main.html',
+      controller: 'Calculator'
+    })
+    .state('showCombination',{
+      url: '/showCombination',
+      templateUrl: 'views/showCombination.html',
+      controller: 'Calculator'
+    })
 });
 
 /*app.controller('Calculator', function($scope){
--- www/js/calculator.js
+++ www/js/calculator.js
@@ -1,25 +1,32 @@
-app.controller('Calculator',['$scope', function($scope){
+app.controller('Calculator', ['$scope', '$state', function($scope, $state){
 	console.log('Start calculator algo!');
-		$scope.startAlgo = function(num){
-		console.log('start Algo...');
-		console.log(num);
-		var min = 1;
-		var max = num - 2;
-		var total = 0;
-		var buf = [];
-		for(var i = 0; i < 2; i++){
-			//random twice, the remaining would be the last value
-			buf[i] = Math.floor((Math.random() * max) + min);
-			max = max - buf[i];
-			total = total + buf[i];
-			console.log(buf[i]);
-			console.log('Total: ' + total);
-		}
-		console.log(total);
-		buf[2] = num - total;
-		console.log('Final buf: ' + buf);
-		console.log('Done');
+	$scope.startAlgo = function(num){
+	console.log('start Algo...');
+	console.log(num);
+	var min = 1;
+	var max = num - 2;
+	var total = 0;
+	var buf = [];
+	$scope.finalTen = [];
+	for(var y = 0; y < 10; y++){
+	  for(var i = 0; i < 2; i++){
+		//random twice, the remaining would be the last value
+		buf[i] = Math.floor((Math.random() * max) + min);
+		max = max - buf[i];
+		total = total + buf[i];
+		console.log(buf[i]);
+		console.log('Total: ' + total);
+	}
+	  console.log(total);
+	  buf[2] = num - total;
+	  console.log('Final buf: ' + buf);
+	  console.log('Done');
+	  $scope.finalTen.push(buf);
 
-		
-	};
+	}
+	console.log($scope.finalTen);
+	$state.go('showCombination')
+
+	
+};
 }]);
