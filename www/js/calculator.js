f5e19233130051ae16a0db3b3907386fd7528745
genliang <lim_genliang@hotmail.com>
2016-06-21 17:58:00 +0800

    Numbers tally. Working version

--- www/js/calculator.js
+++ www/js/calculator.js
@@ -3,15 +3,22 @@ app.controller('Calculator',['$scope', function($scope){
 		$scope.startAlgo = function(num){
 		console.log('start Algo...');
 		console.log(num);
-		var total;
+		var min = 1;
+		var max = num - 2;
+		var total = 0;
 		var buf = [];
-		while(total !== num){
-			for(var i = 0; i < 3; i++){
-				buf[i] = Math.floor((Math.random() * 10) + 1);
-				total = total + buf[i];
-			}
-
+		for(var i = 0; i < 2; i++){
+			//random twice, the remaining would be the last value
+			buf[i] = Math.floor((Math.random() * max) + min);
+			max = max - buf[i];
+			total = total + buf[i];
+			console.log(buf[i]);
+			console.log('Total: ' + total);
 		}
+		console.log(total);
+		buf[2] = num - total;
+		console.log('Final buf: ' + buf);
+		console.log('Done');
 
 		
 	};
